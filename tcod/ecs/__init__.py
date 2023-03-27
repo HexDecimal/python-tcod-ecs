d3abdb339c4451caca222f4f3bc99300985ece94
Kyle Benesch <4b796c65+github@gmail.com>
2023-03-26 18:13:38 -0700

    Prepare 0.1.0 release.

--- tcod/ecs/__init__.py
+++ tcod/ecs/__init__.py
@@ -1,7 +1,7 @@
 """A type-hinted Entity Component System based on Python dictionaries and sets."""
 from __future__ import annotations
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 from functools import partial
 from typing import (
     AbstractSet,
