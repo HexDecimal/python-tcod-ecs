b2d732463fa5977a6a80d01f4f6dab75d4241c19
Kyle Benesch <4b796c65+github@gmail.com>
2023-07-26 00:45:13 -0700

    Update releases workflow
    Copy this for now.
    I might centralize this later.

---
+++ scripts/get_release_description.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Print the description used for GitHub Releases."""
+from __future__ import annotations
+
+import re
+from pathlib import Path
+
+TAG_BANNER = r"## \[[\w.]*\] - \d+-\d+-\d+\n"
+
+RE_BODY = re.compile(rf".*?{TAG_BANNER}(.*?){TAG_BANNER}", re.DOTALL)
+
+
+def main() -> None:
+    """Output the most recently tagged changelog body to stdout."""
+    match = RE_BODY.match(Path("CHANGELOG.md").read_text(encoding="utf-8"))
+    assert match
+    body = match.groups()[0].strip()
+
+    print(body)
+
+
+if __name__ == "__main__":
+    main()
